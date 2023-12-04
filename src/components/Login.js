c6fff13c45554aa951c0c9a9e5ccaded4c1f734f
VAIBHAV SRIVASTAVA <86517958+myhacktech@users.noreply.github.com>
2023-12-04 15:14:22 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -25,7 +25,7 @@ function Login(props) {
     else
     setLoad(true);
 
-    const response = await fetch("api/auth/login", {
+    const response = await fetch("https://backend1-orcin.vercel.app/api/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
